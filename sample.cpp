cd9906d3ea99ac476a1d6b5da24d821fdab44425
song <841194987@qq.com>
2017-09-04 22:29:30 +0800

    create a git project

---
+++ sample.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+
+	printf("Hello, World ! \n That Word is first words of all language in program！ \n");
+	printf("argc is %d",argc);
+
+
+	puts(argv);
+
+
+
+	return 0;
+}
